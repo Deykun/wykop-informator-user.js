e01069282858086329151b6fcd0ea172f6fb1c78
Szymon T <stdeykun+git@gmail.com>
2020-04-09 00:40:07 +0200

    Render of mini menu, appending styles logic
    Localstorage state schema draft

--- informator.user.js
+++ informator.user.js
@@ -38,7 +38,7 @@ const STATES = [
 		color: '#62a2b1',
 	},
 	{
-		code: 'new',
+		code: 'fresh',
 		name: 'Nowe',
 		color: '#8cb1ba',
 	},
@@ -49,36 +49,158 @@ const STATES = [
 	},
 ]
 
+const LSinitState = {
+	total: { },
+	reasons: {
+
+	},
+	mods: {
+
+	}
+}
+STATES.forEach( s => {
+	LSinitState.total[s.code] = 0
+})
+
+// const LSexampleState = {
+// 	total: {
+// 	},
+// 	reasons: {
+// 		nieprawidlowe_tagi: {
+// 			title,
+// 			success,
+// 			fail,
+// 			consultation,
+// 			mods: {
+// 				mtm: {
+// 					success,
+// 					fail,
+// 					consultation
+// 				}
+// 			}
+// 		}
+// 	},
+// 	mods: {
+// 		success,
+// 		fail,
+// 		consultation
+// 	}
+// }
+
+const appendCSS = ( styles ) => {
+	const style = document.createElement('style')
+	style.innerHTML = styles
+	document.head.append(style)
+}
+
+const appendColorsCSS = ( ) => {
+	let colorsCSS = ''
+	STATES.forEach( s => {
+		colorsCSS += `
+			.in-txt-${s.code} { color: ${s.color} !important; }
+			.in-bg-${s.code} { background-color: ${s.color} !important; }
+		`
+	})
+
+	appendCSS( colorsCSS )
+}
+
+const getList = () => {
+	const list = []
+	document.getElementById('violationsList').querySelectorAll('tbody tr').forEach( vTr => {
+		const id = vTr.querySelector('td:nth-child(3) p').textContent.split(':').reverse().pop()
+		const status = vTr.querySelector('td:nth-child(4) .fbold').textContent
+		const reason = vTr.querySelector('td:nth-child(3) .fbold').textContent
+		const moderator = vTr.querySelector('td:nth-child(4) .fbold + span').textContent.split('przez').pop().trim()
+		const date = vTr.querySelector('td:nth-child(4) time') ? vTr.querySelector('td:nth-child(4) time').getAttribute('datetime') : null
+		
+		const violation = {
+			id: id,
+			moderator: moderator, 
+			status: STATES.find( s => s.name === status ).code,
+			reason: reason,
+			date: date,
+		}
+
+		vTr.id = `v-${id}`
+
+		list.push( violation )
+	})
+	return list
+}
+
+const getVolationsStats = ( violations ) => {
+	// const checked = getAlreadyChecked()
+	// const inConsultation = getInConsultation()
+	const total = {}
+	const change = {}
+	STATES.forEach( s => {
+		total[s.code] = 1
+		change[s.code] = 2
+	})
+
+	violations.forEach( v => {
+		total[v.status]++
+	})
+
+	return {
+		total: total,
+		change: Object.keys( change ).some( idx => change[idx] > 0 ) ? change : null
+	}
+}
+
+const renderMiniStats = ( stats ) => {
+	appendColorsCSS()
+	appendCSS(`
+		.in-m__box {
+			display: inline-block;
+			height: 13px;
+			min-width: 13px;
+			vertical-align: middle;
+			border-radius: 10px;
+			margin: 0 6px;
+		}
+		.in-m__value {
+			vertical-align: middle;
+		}
+		.in-m > :last-child {
+			margin-right: 25px;
+		}
+	`)
+	let linkToStats
+	if ( stats.total ) {
+		console.log(stats.total)
+		let statsHTML = ''
+		Object.keys( stats.total ).forEach( status => {
+			const value = stats.total[status]
+			if ( value > 0 ) {
+				const valueEl = `<span class="in-m__value in-m__value--${status}">${value}</span>`
+				const boxEl = `<span class="in-m__box in-bg-${status}"></span>`
+				statsHTML += `<span class="in-m">${status === 'fail' ? boxEl+valueEl : valueEl+boxEl}</span>`
+			}
+		})
+		linkToStats = `<li>
+			<a href="http://www.wykop.pl/naruszenia/informator">
+				<div class="in-mini-stat">
+					${statsHTML}
+				</div>
+			</a>
+		</liv>`
+	} else {
+		linkToStats = '<li><a href="http://www.wykop.pl/naruszenia/informator">Statystyki</a></liv>'
+	}
+	document.querySelector('.bspace > ul:last-child').innerHTML += linkToStats
+}
 
 if ( document.location.pathname.match('/naruszenia/moje') ) {
-	const getList = () => {
-		const list = []
-		document.getElementById('violationsList').querySelectorAll('tbody tr').forEach( vTr => {
-			const id = vTr.querySelector('td:nth-child(3) p').textContent.split(':').reverse().pop()
-			const status = vTr.querySelector('td:nth-child(4) .fbold').textContent
-			const reason = vTr.querySelector('td:nth-child(3) .fbold').textContent
-			const moderator = vTr.querySelector('td:nth-child(4) .fbold + span').textContent.split('przez').pop().trim()
-			const date = vTr.querySelector('td:nth-child(4) time') ? vTr.querySelector('td:nth-child(4) time').getAttribute('datetime') : null
-			
-			const violation = {
-				id: id,
-				moderator: moderator, 
-				status: STATES.find( s => s.name === status ).code,
-				reason: reason,
-				date: date,
-			}
-
-			vTr.id = `v-${id}`
-
-			list.push( violation )
-		})
-		return list
-	}
+	
 	const violations = getList()
 	console.log( violations )
-	// stats = getVolationsStats( violations )
-	// stats -> { total: { }, change: { }} - will be also used in Ajax call for check
-	// if ( stats.change ) save
-	// renderStats( stats )
+	const stats = getVolationsStats( violations )
+	console.log( stats )
+	if ( stats.change ) {
+		// Saving change
+	}
+	renderMiniStats( stats )
 }
 	
